1aadb719c24636f32c43239a154acd03231066f2
Omkar510 <42720133+Omkar510@users.noreply.github.com>
2022-11-19 22:12:29 +0530

    Changes in index file h1 tag text value

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello World!</h1>
+      <h1>Hello Omkar</h1>
     </div>
   );
 }
